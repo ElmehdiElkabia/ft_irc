a57dd51491e9ab749ea7b4d3bb738dfa1b17e3ee
elmehdi  elkabia <elmehdielkabi@gmail.com>
2026-06-04 16:27:15 +0100

    Add Server class definition with member variables and methods

---
+++ include/Server.hpp
@@ -0,0 +1,35 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include "Client.hpp"
+#include "Channel.hpp"
+#include <iostream>
+#include <map>
+
+
+class Client;
+class Channel;
+
+class Server
+{
+    private:
+        std::map<int, Client*>             clients;
+        std::map<std::string, Channel*>    channels;
+        std::string                        password;
+
+    public:
+        Server();
+        ~Server();
+
+        // Client* getClient(int fd);
+        // Client* getClientByNick(const std::string& nick);
+
+        // Channel* getChannel(const std::string& name);
+
+        // void addClient(Client* client);
+        // void removeClient(int fd);
+
+        // void addChannel(Channel* channel);
+};
+
+#endif
