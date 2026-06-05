59989e01d7f364ec184dbe696fe66d2712d0c6a6
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-05 15:05:21 +0100

    Refactor Server class to include Utils.hpp and streamline command handling

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,5 +1,5 @@
 #include "../include/Server.hpp"
-#include "../include/Command.hpp"
+#include "../include/Utils.hpp"
 
 
 Server::Server()
@@ -15,4 +15,14 @@ Server::~Server()
 void Server::handleCommand(Client* client, const std::string& commandLine)
 {
     Command cmd = parseCommand(commandLine);
+
+    if (cmd.command == "PASS")
+        passCommand(client, cmd.params);
+    else if (cmd.command == "NICK")
+        nickCommand(client, cmd.params);
+    else if (cmd.command == "USER")
+        userCommand(client, cmd.params);
+    else
+        std::cout << "Unknown command: " << cmd.command << std::endl;
+
 }
