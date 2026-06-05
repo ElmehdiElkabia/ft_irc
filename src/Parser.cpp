acf4ebfca653a90cf32b50312c2cc243f5fba538
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-05 15:05:14 +0100

    Implement parseCommand function to handle command parsing in Parser class

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,8 +1,27 @@
-#include "../include/Command.hpp"
+#include "../include/Utils.hpp"
 
 Command parseCommand(const std::string &commandLine)
 {
     Command cmd;
 
+    size_t pos = commandLine.find(' ');
+    if (pos == std::string::npos)
+    {
+        cmd.command = commandLine;
+    }
+    else
+    {
+        cmd.command = commandLine.substr(0, pos);
+        std::string paramsStr = commandLine.substr(pos + 1);
+        size_t start = 0;
+        while (start < paramsStr.length())
+        {
+            size_t end = paramsStr.find(' ', start);
+            if (end == std::string::npos)
+                end = paramsStr.length();
+            cmd.params.push_back(paramsStr.substr(start, end - start));
+            start = end + 1;
+        }
+    }
     return cmd;
 }
