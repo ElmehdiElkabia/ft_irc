d9c3540a76e28c02aa73f01aff20656155c9c8e5
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-05 16:36:10 +0100

    Fix parsing logic to handle parameters prefixed with ':' in parseCommand

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -16,11 +16,18 @@ Command parseCommand(const std::string &commandLine)
         size_t start = 0;
         while (start < paramsStr.length())
         {
+            if (paramsStr[start] == ':')
+            {
+                cmd.params.push_back(paramsStr.substr(start + 1));
+                break;
+            }
             size_t end = paramsStr.find(' ', start);
             if (end == std::string::npos)
                 end = paramsStr.length();
             cmd.params.push_back(paramsStr.substr(start, end - start));
             start = end + 1;
+            while (start < paramsStr.length() && paramsStr[start] == ' ')
+                start++;
         }
     }
     return cmd;
