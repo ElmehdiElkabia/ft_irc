8a985c71d8864f3b8a33b03763d751eed39ed969
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-05 15:05:09 +0100

    Implement passCommand function for password authentication in Server class

---
+++ src/commands/auth.cpp
@@ -0,0 +1,27 @@
+#include "../include/Server.hpp"
+
+void Server::passCommand(Client *client, const std::vector<std::string> &params)
+{
+    if (params.size() != 1)
+    {
+        std::cerr << "Usage: PASS <password>" << std::endl;
+        return;
+    }
+    if (client->hasPassed())
+    {
+        std::cerr << "You have already entered the password." << std::endl;
+        return;
+    }
+    if (client->isRegistered())
+    {
+        std::cerr << "You are already registered." << std::endl;
+        return;
+    }
+    std::string password = params[0];
+    if (password != this->password)
+    {
+        std::cerr << "Incorrect password." << std::endl;
+        return;
+    }
+    client->setPassAccepted(true);
+}
