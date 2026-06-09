83a2be0e6a8538ca6d6e96e011b8afe2b7414fe5
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-09 19:29:27 +0100

    Refactor Server class methods for consistency and readability

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,7 +1,6 @@
 #include "../include/Server.hpp"
 #include "../include/Utils.hpp"
 
-
 Server::Server()
 {
     this->password = "secret";
@@ -11,8 +10,7 @@ Server::~Server()
 {
 }
 
-
-Client* Server::getClientByNick(const std::string& nick)
+Client *Server::getClientByNick(const std::string &nick)
 {
     for (std::map<int, Client *>::iterator it = clients.begin(); it != clients.end(); ++it)
     {
@@ -22,10 +20,19 @@ Client* Server::getClientByNick(const std::string& nick)
     return NULL;
 }
 
+Channel *Server::getChannel(const std::string &name)
+{
+    std::map<std::string, Channel *>::iterator it = channels.find(name);
+    if (it != channels.end())
+        return it->second;
+    return NULL;
+}
 
-void Server::handleCommand(Client* client, const std::string& commandLine)
+void Server::handleCommand(Client *client, const std::string &commandLine)
 {
     Command cmd = parseCommand(commandLine);
+
+    
 
     if (cmd.command == "PASS")
         passCommand(client, cmd.params);
@@ -33,7 +40,21 @@ void Server::handleCommand(Client* client, const std::string& commandLine)
         nickCommand(client, cmd.params);
     else if (cmd.command == "USER")
         userCommand(client, cmd.params);
+    else if (cmd.command == "JOIN")
+        joinCommand(client, cmd.params);
+    else if (cmd.command == "PART")
+        partCommand(client, cmd.params);
+    else if (cmd.command == "QUIT")
+        quitCommnand(client, cmd.params);
+    else if (cmd.command == "PRIVMSG")
+        privmsgCommand(client, cmd.params);
+    else if(cmd.command == "TOPIC")
+        topicCommand(client, cmd.params);
     else
         std::cout << "Unknown command: " << cmd.command << std::endl;
+}
 
+void Server::addClient(Client *client)
+{
+    clients[client->getFd()] = client;
 }
