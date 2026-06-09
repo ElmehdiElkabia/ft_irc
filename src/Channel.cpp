8ddfc7c1b89511577cf763d26442f83a670a6a28
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-09 19:28:59 +0100

    Refactor Channel class constructor and member functions for improved readability and consistency

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,36 +1,91 @@
 #include "../include/Channel.hpp"
 
-
-
-Channel::Channel() :
-    name(""),
-    topic("")
+Channel::Channel() : name(""),
+                     topic("")
 {
+    inviteOnly = false;
+    topicRestricted = false;
+    password = "";
+    userLimit = 0;
 }
 
-Channel::Channel(const std::string& name) :
-    name(name),
-    topic("")
+Channel::Channel(const std::string &name) : name(name),
+                                            topic("")
 {
+    inviteOnly = false;
+    topicRestricted = false;
+    password = "";
+    userLimit = 0;
 }
 
 Channel::~Channel()
 {
 }
 
-const std::string& Channel::getName() const
+const std::string &Channel::getName() const
 {
     return name;
 }
 
-const std::string& Channel::getTopic() const
+const std::string &Channel::getTopic() const
 {
     return topic;
 }
 
-void Channel::setTopic(const std::string& topic)
+void Channel::setTopic(const std::string &topic)
 {
     this->topic = topic;
 }
 
+void Channel::addMember(Client *client)
+{
+    members.push_back(client);
+}
 
+void Channel::removeMember(Client *client)
+{
+    for (std::vector<Client *>::iterator it = members.begin(); it != members.end(); ++it)
+    {
+        if (*it == client)
+        {
+            members.erase(it);
+            break;
+        }
+    }
+}
+
+bool Channel::isMember(Client *client) const
+{
+    for (std::vector<Client *>::const_iterator it = members.begin(); it != members.end(); ++it)
+    {
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+bool Channel::isOperator(Client *client) const
+{
+    for (std::vector<Client *>::const_iterator it = operators.begin(); it != operators.end(); ++it)
+    {
+        if (*it == client)
+            return true;
+    }
+    return false;
+}
+
+size_t Channel::memberCount() const
+{
+    return members.size();
+}
+
+
+std::vector<Client *> &Channel::getMembers()
+{
+    return members;
+}
+
+const std::vector<Client *> &Channel::getMembers() const
+{
+    return members;
+}
