abd3742563b18ac88f24ea1bbb15785e509035b8
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-09 19:29:22 +0100

    Refactor main function to initialize multiple clients and streamline command handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,31 @@
 int main()
 {
     Server server;
-    Client client;
-    server.handleCommand(&client, "PASS secret");
-    server.handleCommand(&client, "NICK mehdi");
-    server.handleCommand(&client, "USER mehdi 0 * :Elmehdi elkabia");
+    Client c1;
+    Client c2;
+    // server.handleCommand(&c1, "PASS secret");
+    // server.handleCommand(&c1, "NICK mehdi");
+    // server.handleCommand(&c1, "USER mehdi 0 * :Elmehdi elkabia");
 
-    
+    server.addClient(&c1);
+    server.addClient(&c2);
+
+    server.handleCommand(&c1, "PASS secret");
+    server.handleCommand(&c1, "NICK mehdi");
+    server.handleCommand(&c1, "USER mehdi 0 * :Mehdi");
+
+    server.handleCommand(&c1, "JOIN #42");
+
+    server.handleCommand(&c2, "PASS secret");
+    server.handleCommand(&c2, "NICK ali");
+    server.handleCommand(&c2, "USER ali 0 * :Ali");
+
+    server.handleCommand(&c2, "JOIN #42");
+
+    // server.handleCommand(&c1, "PRIVMSG #42 :Hello everyone!");
+    server.handleCommand(&c1, "PRIVMSG ali :hello");
+
+    // server.handleCommand(&c1, "PART #42");
+
     return 0;
 }
