7c43c83095392159010e4c96fa8d64e0a06432a3
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-15 16:31:41 +0100

    Refactor error handling in topic, invite, and kick commands to use sendToClient for client notifications

--- src/commands/operator.cpp
+++ src/commands/operator.cpp
@@ -14,8 +14,7 @@ void Server::topicCommand(Client *client, const std::vector<std::string> &params
     }
     if (params.size() > 2)
     {
-        std::cerr << "Too many parameters for TOPIC command."
-                  << std::endl;
+        sendToClient(client, "ERROR :Too many parameters for TOPIC command\r\n");
         return;
     }
     std::string channelName = params[0];
@@ -49,19 +48,19 @@ void Server::inviteCommand(Client *client, const std::vector<std::string> &param
 {
     if (!client->isRegistered())
     {
-        std::cerr << "You must be registered to invite someone to a channel." << std::endl;
+        sendToClient(client, "ERROR :You must be registered to invite someone to a channel\r\n");
         return;
     }
     if (params.size() != 2)
     {
-        std::cerr << "Invalid number of parameters for INVITE command." << std::endl;
+        sendToClient(client, "ERROR :Invalid number of parameters for INVITE command\r\n");
         return;
     }
     std::string nickname = params[0];
     Client *invitee = getClientByNick(nickname);
     if (!invitee)
     {
-        std::cerr << "No such user: " << nickname << std::endl;
+        sendToClient(client, "ERROR :No such user: " + nickname + "\r\n");
         return;
     }
     std::string channelName = params[1];
@@ -83,160 +82,65 @@ void Server::inviteCommand(Client *client, const std::vector<std::string> &param
     }
     if (channel->isMember(invitee))
     {
-        std::cerr << "User " << nickname << " is already a member of channel:" << channelName << std::endl;
+        sendToClient(client, "ERROR :User " + nickname + " is already a member of channel: " + channelName + "\r\n");
         return;
     }
     if (channel->isInvited(invitee))
     {
-        std::cerr << "User " << nickname << " has already been invited to channel: " << channelName << std::endl;
+        sendToClient(client, "ERROR :User " + nickname + " has already been invited to channel: " + channelName + "\r\n");
         return;
     }
     channel->addInvited(invitee);
-    std::cout << "User " << nickname << " has been invited to channel " << channelName << "." << std::endl;
+    sendToClient(client, "User " + nickname + " has been invited to channel " + channelName + "." + "\r\n");
 }
 
 void Server::kickCommand(Client *client, const std::vector<std::string> &params)
 {
     if (!client->isRegistered())
     {
-        std::cerr << "You must be registered to kick someone from a channel." << std::endl;
+        sendToClient(client, "ERROR :You must be registered to kick someone from a channel\r\n");
         return;
     }
     if (params.size() != 2)
     {
-        if (channel->getTopic().empty())
-            std::cout << "No topic is set for channel "
-                      << channelName << std::endl;
-        else
-            std::cout << "Current topic for channel "
-                      << channelName << ": "
-                      << channel->getTopic() << std::endl;
-
-        return;
-    }
-
-    if (params.size() == 2)
-    {
-        if (channel->isTopicRestricted() && !channel->isOperator(client))
-        {
-            std::cerr << "Only operators can set the topic."
-                      << std::endl;
-            return;
-        }
-
-        channel->setTopic(params[1]);
-
-        std::cout << "Topic for channel "
-                  << channelName
-                  << " set to: "
-                  << params[1]
-                  << std::endl;
-
-        sendToClient(client, "TOPIC " + channelName + " :" + channel->getTopic() + "\r\n");
-        return;
-    }
-    std::string newTopic = params[1];
-    channel->setTopic(newTopic);
-    sendToClient(client, "TOPIC " + channelName + " :" + newTopic + "\r\n");
-}
-
-void Server::inviteCommand(Client *client, const std::vector<std::string> &params)
-{
-    if (!client->isRegistered())
-    {
-        std::cerr << "You must be registered to invite someone to a channel." << std::endl;
-        return;
-    }
-    if (params.size() != 2)
-    {
-        std::cerr << "Invalid number of parameters for INVITE command." << std::endl;
-        return;
-    }
-    std::string nickname = params[0];
-    Client *invitee = getClientByNick(nickname);
-    if (!invitee)
-    {
-        std::cerr << "No such user: " << nickname << std::endl;
-        return;
-    }
-    std::string channelName = params[1];
-    if (channelName[0] != '#' || channelName.empty())
-    {
-        std::cerr << "Invalid channel name. Channel names must start with '#'." << std::endl;
-        return;
-    }
-    Channel *channel = getChannel(channelName);
-    if (!channel)
-    {
-        std::cerr << "No such channel: " << channelName << std::endl;
-        return;
-    }
-    if (!channel->isMember(client))
-    {
-        std::cerr << "You are not a member of channel: " << channelName << std::endl;
-        return;
-    }
-    if (channel->isMember(invitee))
-    {
-        std::cerr << "User " << nickname << " is already a member of channel:" << channelName << std::endl;
-        return;
-    }
-    if (channel->isInvited(invitee))
-    {
-        std::cerr << "User " << nickname << " has already been invited to channel: " << channelName << std::endl;
-        return;
-    }
-    channel->addInvited(invitee);
-    std::cout << "User " << nickname << " has been invited to channel " << channelName << "." << std::endl;
-}
-
-void Server::kickCommand(Client *client, const std::vector<std::string> &params)
-{
-    if (!client->isRegistered())
-    {
-        std::cerr << "You must be registered to kick someone from a channel." << std::endl;
-        return;
-    }
-    if (params.size() != 2)
-    {
-        std::cerr << "Invalid number of parameters for KICK command." << std::endl;
+        sendToClient(client, "ERROR :Invalid number of parameters for KICK command\r\n");
         return;
     }
 
     std::string channelName = params[0];
     if (channelName[0] != '#' || channelName.empty())
     {
-        std::cerr << "Invalid channel name. Channel names must start with '#'." << std ::endl;
+        sendToClient(client, "ERROR :Invalid channel name. Channel names must start with '#'.\r\n");
         return;
     }
     Channel *channel = getChannel(channelName);
     if (!channel)
     {
-        std::cerr << "No such channel: " << channelName << std::endl;
+        sendToClient(client, "ERROR :No such channel: " + channelName + "\r\n");
         return;
     }
     std::string nickname = params[1];
     Client *kickee = getClientByNick(nickname);
     if (!kickee)
     {
-        std::cerr << "No such user: " << nickname << std::endl;
+        sendToClient(client, "ERROR :No such user: " + nickname + "\r\n");
         return;
     }
     if (!channel->isMember(client))
     {
-        std::cerr << "You are not a member of channel: " << channelName << std::endl;
+        sendToClient(client, "ERROR :You are not a member of channel: " + channelName + "\r\n");
         return;
     }
     if (!channel->isOperator(client))
     {
-        std::cerr << "You are not an operator of channel: " << channelName << std::endl;
+        sendToClient(client, "ERROR :You must be an operator to kick someone from channel: " + channelName + "\r\n");
         return;
     }
     if (!channel->isMember(kickee))
     {
-        std::cerr << "User " << nickname << " is not a member of channel: " << channelName << std::endl;
+        sendToClient(client, "ERROR :User " + nickname + " is not a member of channel: " + channelName + "\r\n");
         return;
     }
     channel->removeMember(kickee);
-    std::cout << "User " << nickname << " has been kicked from channel " << channelName << "." << std::endl;
+    sendToClient(kickee, "You have been kicked from channel: " + channelName + "\r\n");
 }
