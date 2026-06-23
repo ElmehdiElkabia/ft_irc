446827a14d6ad132050017b1c8326f6571dc4ae7
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-06-23 09:52:45 +0100

    Refactor join and part command error handling to use standardized error responses

--- src/commands/channel.cpp
+++ src/commands/channel.cpp
@@ -5,14 +5,15 @@ void Server::joinCommand(Client *client,
 {
     if (!client->isRegistered())
     {
-        sendToClient(client, "ERROR :You must be registered to join a channel\r\n");
-
+        sendToClient(client,
+                     ERR_NOTREGISTERED(client->getNickname()) + "\r\n");
         return;
     }
 
-    if (params.size() < 1 || params.size() > 2)
+    if (params.empty())
     {
-        sendToClient(client, "ERROR :Invalid number of parameters for JOIN command\r\n");
+        sendToClient(client,
+                     ERR_NEEDMOREPARAMS(client->getNickname(), "JOIN") + "\r\n");
         return;
     }
 
@@ -20,12 +21,14 @@ void Server::joinCommand(Client *client,
 
     if (channelName.empty() || channelName[0] != '#')
     {
-        sendToClient(client, "ERROR :Invalid channel name. Channel names must start with '#'.\r\n");
+        sendToClient(client,
+                     ERR_NOSUCHCHANNEL(client->getNickname(), channelName) + "\r\n");
         return;
     }
 
     Channel *channel = getChannel(channelName);
 
+    // Create channel
     if (!channel)
     {
         channel = new Channel(channelName);
@@ -34,104 +37,154 @@ void Server::joinCommand(Client *client,
         channel->addMember(client);
         channel->addOperator(client);
 
-        sendToClient(client, "Client " + client->getNickname() + " created channel " + channelName + " and became operator." + "\r\n");
+        sendToClient(client,
+                     ":" + client->getNickname() +
+                         " JOIN " + channelName + "\r\n");
+
+        if (channel->getTopic().empty())
+        {
+            sendToClient(client,
+                         RPL_NOTOPIC(client->getNickname(),
+                                     channelName) +
+                             "\r\n");
+        }
+        else
+        {
+            sendToClient(client,
+                         RPL_TOPIC(client->getNickname(),
+                                   channelName,
+                                   channel->getTopic()) +
+                             "\r\n");
+        }
+
         return;
     }
 
     if (channel->isMember(client))
     {
-        sendToClient(client, "ERROR :You are already a member of channel: " + channelName + "\r\n");
         return;
     }
 
+    // MODE +i
     if (channel->isInviteOnly())
     {
         if (!channel->isInvited(client))
         {
-            sendToClient(client, "ERROR :Channel " + channelName + " is invite-only. You must be invited to join.\r\n");
+            sendToClient(client,
+                         ERR_INVITEONLYCHAN(client->getNickname(),
+                                            channelName) +
+                             "\r\n");
             return;
         }
 
         channel->removeInvitation(client);
     }
 
+    // MODE +k
     if (channel->hasKey())
     {
-        if (params.size() < 2)
+        if (params.size() < 2 ||
+            params[1] != channel->getKey())
         {
-            sendToClient(client, "ERROR :Channel " + channelName + " requires a password. Please provide the password to join.\r\n");
-            return;
-        }
-
-        if (params[1] != channel->getKey())
-        {
-            sendToClient(client, "ERROR :Incorrect channel password for channel " + channelName + ".\r\n");
+            sendToClient(client,
+                         ERR_BADCHANNELKEY(client->getNickname(),
+                                           channelName) +
+                             "\r\n");
             return;
         }
     }
 
-    if (channel->getUserLimit() > 0)
+    // MODE +l
+    if (channel->getUserLimit() > 0 &&
+        channel->memberCount() >=
+            static_cast<size_t>(channel->getUserLimit()))
     {
-        if (channel->memberCount() >= static_cast<size_t>(channel->getUserLimit()))
-        {
-            sendToClient(client, "ERROR :Channel " + channelName + " is full. User limit reached.\r\n");
-            return;
-        }
+        sendToClient(client,
+                     ERR_CHANNELISFULL(client->getNickname(),
+                                       channelName) +
+                         "\r\n");
+        return;
     }
 
     channel->addMember(client);
-    sendToClient(client, "Client " + client->getNickname() + " joined channel " + channelName + "." + "\r\n");
+
+    sendToClient(client,
+                 ":" + client->getNickname() +
+                     " JOIN " + channelName + "\r\n");
+
+    if (channel->getTopic().empty())
+    {
+        sendToClient(client,
+                     RPL_NOTOPIC(client->getNickname(),
+                                 channelName) +
+                         "\r\n");
+    }
+    else
+    {
+        sendToClient(client,
+                     RPL_TOPIC(client->getNickname(),
+                               channelName,
+                               channel->getTopic()) +
+                         "\r\n");
+    }
 }
 
-void Server::partCommand(Client *client, const std::vector<std::string> &params)
+void Server::partCommand(Client *client,
+                         const std::vector<std::string> &params)
 {
     if (!client->isRegistered())
     {
-        sendToClient(client, "ERROR :You must be registered to part a channel\r\n");
+        sendToClient(client,
+                     ERR_NOTREGISTERED(client->getNickname()) + "\r\n");
         return;
     }
+
     if (params.size() != 1)
     {
-        sendToClient(client, "ERROR :Invalid number of parameters for PART command\r\n");
+        sendToClient(client,
+                     ERR_NEEDMOREPARAMS(client->getNickname(), "PART") + "\r\n");
         return;
     }
+
     std::string channelName = params[0];
-    if (channelName[0] != '#')
+
+    if (channelName.empty() || channelName[0] != '#')
     {
-        sendToClient(client, "ERROR :Invalid channel name. Channel names must start with '#'.\r\n");
+        sendToClient(client,
+                     ERR_NOSUCHCHANNEL(client->getNickname(),
+                                       channelName) +
+                         "\r\n");
         return;
     }
+
     Channel *channel = getChannel(channelName);
+
     if (!channel)
     {
-        sendToClient(client, "ERROR :No such channel: " + channelName + "\r\n");
+        sendToClient(client,
+                     ERR_NOSUCHCHANNEL(client->getNickname(),
+                                       channelName) +
+                         "\r\n");
         return;
     }
+
     if (!channel->isMember(client))
     {
-        sendToClient(client, "ERROR :You are not a member of channel: " + channelName + "\r\n");
+        sendToClient(client,
+                     ERR_NOTONCHANNEL(client->getNickname(),
+                                      channelName) +
+                         "\r\n");
         return;
     }
+
+    sendToClient(client,
+                 ":" + client->getNickname() + " PART " + channelName + "\r\n");
+
     channel->removeMember(client);
-    if (channel->memberCount() == 1)
+
+    if (channel->memberCount() == 0)
     {
-        delete channel;
         channels.erase(channelName);
-        sendToClient(client, "Channel " + channelName + " has been deleted as it has no more members.\r\n");
-        return;
+        delete channel;
     }
-    sendToClient(client, "You have left channel: " + channelName + "\r\n");
-}
-
-void Server::quitCommand(Client *client, const std::vector<std::string> &params)
-{
-
-    (void)params;
-    // you check if the client is registerd
-    // you remove the client from all channels they are a member
-    // you delete the client from the server's client list
-    // you should close the socket of the client
-    // okay a khay abdo
-    // rah dart likk koulchii hena
-    sendToClient(client, "You are quitting the IRC server.\r\n");
 }
